89efcccd6d95033047961cc25635d4cd00b9fb5b
Zerik20 <erikaban14@gmail.com>
2024-08-22 16:21:31 -0600

    Ya funciona el i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), astroI18next(),]
 });
